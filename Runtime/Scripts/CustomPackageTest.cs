7a09ec88b9ce64fbad96e9f5e36c9e3e6c30d5bc
İrfan Yiğit BAYSAL <43708297+irfanbaysal@users.noreply.github.com>
2024-05-11 12:47:36 +0300

    Add files via upload

---
+++ Runtime/Scripts/CustomPackageTest.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace CustomPackageTest
+{
+    public class CustomPackageTest : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log($"{this.name} is started");
+        }
+    }    
+}
+
